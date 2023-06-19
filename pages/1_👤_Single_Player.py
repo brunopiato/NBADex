acfb0f98ab82355e7308ab695a999c13e69a483f
brunopiato <piatobio@gmail.com>
2023-06-19 12:55:24 -0300

    shot chart v0.1

--- pages/1_👤_Single_Player.py
+++ pages/1_👤_Single_Player.py
@@ -230,7 +230,7 @@ st.metric(label='',
     # ----------------------------------------------------------------    
     
 with st.container():
-    col1, col2, col3, col4, col5 = st.columns([2, 5, 5, 3, 3])
+    col1, col2, col3, col4, col5 = st.columns([1, 5, 5, 3, 2])
     with col1:
         st.empty()
     with col2:
